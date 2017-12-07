20fe5c818c2e840a3d2d862c40be2d931c94e6af
Erik-Jan van Kesteren <erikjan.vankesteren@gmail.com>
2017-12-07 13:25:14 +0100

    Remove duplicate code
    Evaluate %<-% function in parent frame of %->% function.

--- R/Massign.R
+++ R/Massign.R
@@ -119,70 +119,10 @@
 #'
 #' @export
 `%->%` <- function(value, var) {
-  # argument checking
-  if (!is.character(value)) {
-    stop("Please enter a proper character matrix. See ?`%<-%`")
-  }
-
-  # first, remove trailing commas if they exist
-  txt <- gsub("\\,[^0-9]*(?=\\n)", "", value, perl = TRUE)
-
-  # then extract the rows
-  rows <- strsplit(txt, "[\n\\\\]", perl = TRUE)[[1]]
-
-  # calculate nrow and ncol using number of commas
-  commas <- sapply(gregexpr(",", rows, fixed = TRUE),
-                   function(x) {
-                     if (x[1] < 0) {
-                       return(0)
-                     } else {
-                       length(x)
-                     }
-                   })
-  commasTotal <- sum(commas)
-
-  nc <- max(commas) + 1
-  nr <- length(rows)
-
-  nValues <- commasTotal + nr
-
-  if (nValues != nr*nc) {
-    isLowerTriangular <- all(commas == seq_along(commas) - 1)
-    isUpperTriangular <- all(commas == rev(seq_along(commas) - 1))
-    if (isLowerTriangular) {
-      # fill the upper triangle
-      for (i in seq_along(rows)) {
-        sRows <- strsplit(rows, ",")
-        rows[[i]] <- paste(sapply(sRows, function(x) x[i]), collapse = ",")
-      }
-    } else if (isUpperTriangular) {
-      stop("Upper triangular matrices not yet supported, use lower triangular.")
-    } else {
-      stop("(Syntax) number of values not equal to number of cells in matrix.")
-    }
-  }
-
-  # create data comma separated values
-  d <- paste(rows, collapse = ",")
-
-  # create matrix expression
-  matExp <- paste0("matrix(data = c(", d, "), byrow = TRUE, nrow = ", nr,
-                   ", ncol = ", nc, ")")
-
-  # Assign to temporary variable. This system is petty hacky but works well,
-  # even for subenvironments and lists etc.
-  # First, get the environment in which the function was called
-  pf <- parent.frame()
-
-  # Then, create a variable with a reserved name
-  eval(parse(text = paste0("`__MassignMat__` <-", matExp)), envir = pf)
-
-  # Next, simply evaluate the assignment in the pf with the newly created var
-  eval(parse(text = paste0(deparse(substitute(var)), " <- `__MassignMat__`")),
-       envir = pf)
-
-  # Lastly, remove the var.
-  evalq(rm("__MassignMat__"), envir = pf)
+  eval(parse(text = paste0("`%<-%`(",
+                           deparse(substitute(var)),
+                           ",'", val,"')")),
+       envir = parent.frame())
 }
 
 
